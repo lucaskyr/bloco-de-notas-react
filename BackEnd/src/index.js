c2a7174df758667e31170db6d9222a50c2431c5d
lucas.oliveira.kyr <131033597+lucaskyr@users.noreply.github.com>
2024-11-03 09:50:20 -0300

    fim back end

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const routes = require('./routes')
-
+const cors = require('cors')
 const app = express();
 
 
 require('./config/mongodbconfig');
 
 
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
